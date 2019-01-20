7a3544f1c9699a00edc1d69b88c52192a152484f
poviigna <povilaitissignas@gmail.com>
2019-01-20 11:18:10 +0200

    Revert "check this out"
    This reverts commit cba6e17c4c95c256b01fdc5fd39890aeee001845.

--- Git/Source.cpp
+++ Git/Source.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main()
 {
-	cout << "Sveikas Gyvuju Pasauli" << endl;
-	cout<<"Kai jausies vienisas spausk cia : "<<"https://www.google.lt/search?rlz=1C1GCEU_ltLT832&biw=1920&bih=920&tbm=isch&sa=1&ei=8jlEXJeuG464kwWjnL-wCA&q=as+merginas+kaip+filmus+nepatinka+isjungiu&oq=as+merginas+kaip+filmus+nepatinka+isjungiu&gs_l=img.3...6544.18044..18239...0.0..1.180.3051.41j1......2....1..gws-wiz-img.....0..35i39j0j0i30j0i19j0i8i30i19j0i24.69oXRHYjbBo#imgrc=fObiFkr2q45-iM:"<<endl;
+	cout << "Hello,World!" << endl;
+	
 	system("pause");
 }
